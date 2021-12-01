44f1ecc39df3fe41ebe1769c70ea3952ca49aa00
Maxim Fedotov <mvfedotov_1@edu.hse.ru>
2021-12-01 19:15:14 +0300

    Update weather.py

--- service/weather.py
+++ service/weather.py
@@ -7,7 +7,7 @@ class Weather:
 	def get_weather() -> dict:
 		API = '8397aa1b69899690124619bad12a067e'
 		id_Moscow = '524901'
-		r = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+id_Moscow+'&appid='+API).json()
+		r = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+id_Moscow+'&appid='+API +'&units=metric').json()
 		# with open('weather_templates.json', 'w') as f:
 		# 	json.dump(r, f)
 		return r
